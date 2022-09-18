8514f9f6bd2e6277756bcf73805c3a84d2fe6540
Vlynn Noo <89231237+Vlynn01@users.noreply.github.com>
2022-09-18 17:37:42 +0700

    Add files via upload

---
+++ Bai-Tap-MD2/src/$9_Automated_Testing_TDD/ThucHanh/UngDungMayTinhDonGian/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package $9_Automated_Testing_TDD.ThucHanh.UngDungMayTinhDonGian;
+
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        return first - second;
+    }
+}
