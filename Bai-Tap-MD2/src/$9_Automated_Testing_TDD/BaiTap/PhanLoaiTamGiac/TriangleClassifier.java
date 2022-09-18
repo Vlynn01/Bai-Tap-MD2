a1541f9d459c21e148388889499d16da7fa9d71c
Vlynn Noo <89231237+Vlynn01@users.noreply.github.com>
2022-09-18 17:58:42 +0700

    Add files via upload

---
+++ Bai-Tap-MD2/src/$9_Automated_Testing_TDD/BaiTap/PhanLoaiTamGiac/TriangleClassifier.java
@@ -0,0 +1,16 @@
+package $9_Automated_Testing_TDD.BaiTap.PhanLoaiTamGiac;
+
+public class TriangleClassifier {
+    public static String edge(double canhA, double canhB, double canhC) {
+        if (canhA == canhB && canhA == canhC) {
+            return "Tam giác đều";
+        }
+        else if ((canhA == canhB) || (canhA == canhC) || (canhB == canhC)){
+            return "Tam giác cân";
+        }else if((canhA + canhB) > canhC || (canhB + canhC) > canhA) {
+            return "Tam giác thường";
+        }else {
+            return "Không phải tam giác";
+        }
+    }
+}
