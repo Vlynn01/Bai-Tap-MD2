80824eab86cb583a2ecb4c13eae9734695bf0495
Vlynn Noo <89231237+Vlynn01@users.noreply.github.com>
2022-09-26 14:22:26 +0700

    Add files via upload

---
+++ Bai-Tap-MD2/src/$12_JavaCollectionFrameWork/CaiDatTimKiemNhiPhan/AbstractTree.java
@@ -0,0 +1,8 @@
+package $12_JavaCollectionFrameWork.Optional.CaiDatTimKiemNhiPhan;
+
+public abstract class AbstractTree<E> implements Tree<E> {
+    @Override /** Inorder traversal from the root*/
+    public void inorder() {
+    }
+}
+
